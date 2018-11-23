77e2be0c99216867eb68590835b01a694b8b2c0b
Michael Hornegger <michael.hornegger@karriere.at>
2018-11-23 06:16:50 +0100

    Add product list to model

--- src/main/kotlin/com/miho/thymeleafcourse/web/IndexController.kt
+++ src/main/kotlin/com/miho/thymeleafcourse/web/IndexController.kt
@@ -1,11 +1,17 @@
 package com.miho.thymeleafcourse.web
 
+import com.miho.thymeleafcourse.service.ProductService
 import org.springframework.stereotype.Controller
+import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.RequestMapping
 
 @Controller
-class IndexController {
+class IndexController(private val productService: ProductService) {
 
     @RequestMapping("/")
-    fun getIndex() = "index"
+    fun getIndex(model: Model): String {
+        model.addAttribute("products", productService.getAllProducts())
+
+        return "index"
+    }
 }
