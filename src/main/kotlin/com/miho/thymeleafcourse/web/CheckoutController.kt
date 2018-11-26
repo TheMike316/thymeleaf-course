25baa618ce17f98aa258177ace054c0cc1660f9e
mikeh <mike.hornegger@gmail.com>
2018-11-26 05:44:22 +0100

    Add error message

--- src/main/kotlin/com/miho/thymeleafcourse/web/CheckoutController.kt
+++ src/main/kotlin/com/miho/thymeleafcourse/web/CheckoutController.kt
@@ -2,6 +2,7 @@ package com.miho.thymeleafcourse.web
 
 import com.miho.thymeleafcourse.command.CheckoutCommand
 import org.springframework.stereotype.Controller
+import org.springframework.ui.Model
 import org.springframework.validation.BindingResult
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
@@ -11,7 +12,11 @@ import javax.validation.Valid
 class CheckoutController {
 
     @GetMapping("/checkout")
-    fun checkoutForm() = "checkout-form"
+    fun checkoutForm(model: Model): String {
+        model.addAttribute("checkoutCommand", CheckoutCommand())
+
+        return "checkout-form"
+    }
 
     @PostMapping("/do-checkout")
     fun doCheckout(@Valid checkoutCommand: CheckoutCommand, bindingResult: BindingResult) =
