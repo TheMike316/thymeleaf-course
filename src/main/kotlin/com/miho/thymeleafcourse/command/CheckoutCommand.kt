fdfdbf386d3202dd80aed4800fefbb1a75838908
mikeh <mike.hornegger@gmail.com>
2018-11-28 05:12:13 +0100

    Set validation annotation on fields

--- src/main/kotlin/com/miho/thymeleafcourse/command/CheckoutCommand.kt
+++ src/main/kotlin/com/miho/thymeleafcourse/command/CheckoutCommand.kt
@@ -3,10 +3,10 @@ package com.miho.thymeleafcourse.command
 import javax.validation.constraints.NotEmpty
 import javax.validation.constraints.Size
 
-data class CheckoutCommand(@NotEmpty @Size(min = 2, max = 50) var firstName: String = "",
-                           @NotEmpty @Size(min = 2, max = 50) var lastName: String = "",
-                           @NotEmpty @Size(min = 2, max = 50) var addressLine1: String = "",
+data class CheckoutCommand(@field: [NotEmpty Size(min = 2, max = 50)] var firstName: String = "",
+                           @field: [NotEmpty Size(min = 2, max = 50)] var lastName: String = "",
+                           @field: [NotEmpty Size(min = 2, max = 50)] var addressLine1: String = "",
                            var addressLine2: String = "",
-                           @NotEmpty @Size(min = 2, max = 50) var city: String = "",
-                           @NotEmpty @Size(min = 2, max = 2) var stateCode: String = "",
-                           @NotEmpty @Size(min = 2, max = 10) var zip: String = "")
+                           @field: [NotEmpty Size(min = 2, max = 50)] var city: String = "",
+                           @field: [NotEmpty Size(min = 2, max = 2)] var stateCode: String = "",
+                           @field: [NotEmpty Size(min = 2, max = 10)] var zip: String = "")
